df6202137cbb68edc426586425e2239f6c7f4243
YuliaBondar <uliabondar359@gmail.com>
2024-07-17 15:16:54 +0300

    SeeMaldives

---
+++ src/component/ExploreMaldives/SeeAllMaldives.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SeeAllMaldives = () => {
+  return (
+    <div className='maldives_class'>
+      <div className="text_maldives">
+        <h1 style={}>Maldives</h1>
+
+      </div>
+    </div>
+  )
+}
+
+export default SeeAllMaldives
